6856fb28cb0ea5cbd1a5f9a6113c3e3b05b69678
kahqgc <kahqgc@gmail.com>
2025-03-13 11:40:15 -0500

    2d-arrays 2

--- 2d-arrays.js
+++ 2d-arrays.js
@@ -5,14 +5,14 @@ let allScores = [
 ]
 console.log(allScores);
 
-theaterSeating = [
+let theaterSeating = [
     ["A1", "A2","A3", "A4"],
     ["B1", "B2", "B3", "B4"],
     ["C1", "C2", "C3", "C4"],
 ]
 console.log(theaterSeating);
 
-contactsList = [
+let contactsList = [
     ["Ken", "kharris@gmail.com", "636-286-2732"],
     ["Lisa", "lharris@gmail.com", "314-748-3829"]
 ]
