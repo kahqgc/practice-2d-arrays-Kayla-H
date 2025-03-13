ecc7400c3d07f81d48c8296b41f556d04b66f5f6
kahqgc <kahqgc@gmail.com>
2025-03-13 11:38:04 -0500

    2d arrays

---
+++ 2d-arrays.js
@@ -0,0 +1,20 @@
+let allScores = [
+    [98,72,88],
+    [70,65,82],
+    [95,86,91],
+]
+console.log(allScores);
+
+theaterSeating = [
+    ["A1", "A2","A3", "A4"],
+    ["B1", "B2", "B3", "B4"],
+    ["C1", "C2", "C3", "C4"],
+]
+console.log(theaterSeating);
+
+contactsList = [
+    ["Ken", "kharris@gmail.com", "636-286-2732"],
+    ["Lisa", "lharris@gmail.com", "314-748-3829"]
+]
+
+console.log(contactsList);
